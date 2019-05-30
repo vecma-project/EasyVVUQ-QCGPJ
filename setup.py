b462e6505c65cdd6ec2fb2be7f5e1b27716db05b
Bartosz Bosak <bbosak@man.poznan.pl>
2019-05-30 14:34:55 +0200

    Moved to the M12 release of EasyVVUQ

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-name='easyvvuq-qcgpj',
+name='EasyVVUQ-QCGPJ',
 
-version='0.0.1.dev1',
+version='0.0.2.dev1',
 
 description='A lightweight wrapper on EasyVVUQ enabling the execution of VVUQ '
 'using the QCG Pilot Job mechanism.',
@@ -11,13 +11,14 @@ description='A lightweight wrapper on EasyVVUQ enabling the execution of VVUQ '
 long_description='A lightweight wrapper on EasyVVUQ enabling the execution of VVUQ '
 'using the QCG Pilot Job mechanism.',
 
-url='TODO',
+url='https://github.com/vecma-project/EasyVVUQ-QCGPJ',
 
 author='Bartosz Bosak',
 
 install_requires=[
+'scipy==1.2.1',
 'qcgpilotmanager @ git+https://github.com/vecma-project/QCG-PilotJob.git@master#egg=qcgpilotmanager',
-'easyvvuq @ git+https://github.com/UCL-CCS/EasyVVUQ.git@qcgpj_integration#egg=easyvvuq',
+'easyvvuq @ git+https://github.com/UCL-CCS/EasyVVUQ.git@master#egg=easyvvuq',
 ],
 
 packages=find_packages(),
