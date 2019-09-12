c2ff7565f8ed301c57a0de4a8395a4c3a0ba1387
Bartosz Bosak <bbosak@man.poznan.pl>
2019-09-12 14:05:04 +0200

    dependence on concrete qcgpj and easyvvuq tags / version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 name='EasyVVUQ-QCGPJ',
 
-version='0.0.3.dev1',
+version='0.0.4.dev1',
 
 description='A lightweight wrapper on EasyVVUQ enabling the execution of VVUQ '
 'using the QCG Pilot Job mechanism.',
@@ -19,10 +19,8 @@ install_requires=[
     'pytest',
     'pytest-pep8',
     'scipy==1.2.1',
-    'qcgPilotManager @ git+https://github.com/vecma-project/QCG-PilotJob.git@issue_33_node_launcher#egg=qcgPilotManager',
-    'easyvvuq @ git+https://github.com/UCL-CCS/EasyVVUQ.git@dev#egg=easyvvuq'
-    #'qcgPilotManager==0.4.1',
-    #'easyvvuq==0.3'
+    'qcgPilotManager @ git+https://github.com/vecma-project/QCG-PilotJob.git@v0.5#egg=qcgPilotManager',
+    'easyvvuq @ git+https://github.com/UCL-CCS/EasyVVUQ.git@v0.4.dev1#egg=easyvvuq'
 ],
 
 packages=find_packages(),
