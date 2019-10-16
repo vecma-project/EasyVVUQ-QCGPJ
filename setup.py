3d235152f2e48ea2375bd8d386c50d76e00a9ea3
Bartosz Bosak <bbosak@man.poznan.pl>
2019-10-16 10:39:58 +0200

    switch to monitoring branch of qcgpj

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires=[
     'pytest',
     'pytest-pep8',
     'scipy==1.2.1',
-    'qcgPilotManager @ git+https://github.com/vecma-project/QCG-PilotJob.git@v0.5#egg=qcgPilotManager',
+    'qcgPilotManager @ git+https://github.com/vecma-project/QCG-PilotJob.git@issue_37_monitoring#egg=qcgPilotManager',
     'easyvvuq @ git+https://github.com/UCL-CCS/EasyVVUQ.git@v0.4#egg=easyvvuq'
 ],
 
