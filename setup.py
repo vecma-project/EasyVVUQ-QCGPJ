ff33ef1ff4e6b993194bd7a1154db2142539735d
Bartosz Bosak <bbosak@man.poznan.pl>
2020-05-06 11:48:20 +0200

    Upgrade to QCGPJ 0.7.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 name='EasyVVUQ-QCGPJ',
 
-version='0.2.0',
+version='0.2.1',
 
 description='A lightweight wrapper on EasyVVUQ enabling the execution of VVUQ '
 'using the QCG Pilot Job mechanism.',
@@ -18,7 +18,7 @@ author='Bartosz Bosak',
 install_requires=[
     'pytest',
     'pytest-pep8',
-    'qcgPilotManager @ git+https://github.com/vecma-project/QCG-PilotJob.git@v0.7.0#egg=qcgPilotManager',
+    'qcgPilotManager @ git+https://github.com/vecma-project/QCG-PilotJob.git@v0.7.1#egg=qcgPilotManager',
     'easyvvuq @ git+https://github.com/UCL-CCS/EasyVVUQ.git@v0.5.1#egg=easyvvuq'
 ],
 
