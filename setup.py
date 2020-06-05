f2662f7f50b81b7b4dbdb6aba83485635401b486
Bartosz Bosak <bbosak@man.poznan.pl>
2020-06-05 14:55:05 +0200

    integration with pypi, v 0.3rc1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 name='EasyVVUQ-QCGPJ',
 
-version='0.2.1',
+version='0.3rc1',
 
 description='A lightweight wrapper on EasyVVUQ enabling the execution of VVUQ '
 'using the QCG Pilot Job mechanism.',
@@ -19,7 +19,7 @@ install_requires=[
     'pytest',
     'pytest-pep8',
     'easyvvuq >= 0.6',
-    'qcg-pilotjob @ git+https://github.com/vecma-project/QCG-PilotJob.git@release24'
+    'qcg-pilotjob == 0.8.0rc4'
 ],
 
 packages=find_packages(),
