cde7f53bf8d56e36a102a28b59f675e2f7c13e0a
Bartosz Bosak <bbosak@man.poznan.pl>
2021-01-07 14:32:38 +0100

    version update (trying to resolve pypi issue)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = readme.split("\n", 2)[2]
 setup(
     name='easyvvuq-qcgpj',
 
-    version='0.5.0rc1',
+    version='0.5rc4',
 
     description='A lightweight plugin for EasyVVUQ enabling the execution of demanding VVUQ procedures '
                 'using the QCG-PilotJob mechanism.',
